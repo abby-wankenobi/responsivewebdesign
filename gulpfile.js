84e332316fe69261a0548030ff37eefd01cd07f5
Abigail Swarth <abigailswarth@gmail.com>
2018-11-18 16:18:21 -0500

    home section background

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,14 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 var browserSync = require('browser-sync').create();
+var autoprefixer = require('gulp-autoprefixer');
+
 
 gulp.task('sass', function(){
-  return gulp.src('app/scss/**/*.scss')
+  return gulp
+  .src('app/scss/**/*.scss')
   .pipe(sass())
+  .pipe(autoprefixer('last 2 versions'))
   .pipe(gulp.dest('app/css'))
   .pipe(browserSync.reload({
     stream: true
@@ -14,7 +18,7 @@ gulp.task('sass', function(){
 gulp.task('watch', ['browserSync', 'sass'], function(){
   gulp.watch('app/scss/**/*.scss',['sass']);
   gulp.watch('app/*.html', browserSync.reload);
-  gulp.watch('app/js/**/*.js', browserSync.reload); 
+  gulp.watch('app/js/**/*.js', browserSync.reload);
 })
 
 gulp.task('browserSync', function(){
